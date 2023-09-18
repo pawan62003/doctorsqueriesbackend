4dac2ae7fdb4eb56a3ead02a412c05fcb3062cdf
pawan kumar <pawan6200327812@gmail.com>
2023-09-18 07:34:20 +0530

    compleated Appointment dynamic userId

--- routes/appointment.route.js
+++ routes/appointment.route.js
@@ -1,16 +1,17 @@
 const express = require("express");
 const AppointmentRoute = express.Router();
 const { AppointMentModel } = require("../models/appointment.model");
+const jwt = require("jsonwebtoken");
 
 AppointmentRoute.get("/", async (req, res) => {
   try {
-    const query = {}
-    const {user,status} = req.query
-    if(user){
-      query.userId = user
+    const query = {};
+    const { user, status } = req.query;
+    if (user) {
+      query.userId = user;
     }
-    if(status){
-      query.status = status
+    if (status) {
+      query.status = status;
     }
 
     const data = await AppointMentModel.find(query);
@@ -21,15 +22,23 @@ AppointmentRoute.get("/", async (req, res) => {
 });
 
 AppointmentRoute.post("/", async (req, res) => {
-    try {
+  try {
+    const { token } = req.body;
+    const decode = jwt.verify(token, "solo_project");
+    if (decode) {
+      req.body.userId = decode.userID;
+      delete req.body.token;
       const newAppointment = new AppointMentModel(req.body);
       const savedAppointment = await newAppointment.save();
-      res.status(201).json({ msg: "Appointment saved successfully", appointment: savedAppointment });
-    } catch (error) {
-      console.error("Error while creating an appointment:", error);
-      res.status(500).json({ msg: "Failed to create an appointment" });
+      res.status(201).json({ msg: "Appointment saved successfully" });
+    } else {
+      res.send({ msg: "Please Login !!!" });
     }
-  });
+  } catch (error) {
+    console.error("Error while creating an appointment:", error);
+    res.status(500).json({ msg: "Failed to create an appointment" });
+  }
+});
 
 AppointmentRoute.get("/:id", async (req, res) => {
   try {
