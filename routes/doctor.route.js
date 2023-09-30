bffab4bf33a427222db53325496bd045f9a666f7
pawan kumar <pawan6200327812@gmail.com>
2023-09-30 16:44:27 +0530

    done

--- routes/doctor.route.js
+++ routes/doctor.route.js
@@ -160,7 +160,7 @@ DoctorRoute.get("/doctors/near", async (req, res) => {
           location.latitude,
           location.longitude
         );
-        if (distance <= 80) {
+        if (distance <= 280) {
           person.set("distance", distance);
           distances.push({ person, distance });
         }
