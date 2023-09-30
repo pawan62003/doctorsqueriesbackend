f3251db4a9fe1525edf4fef696f960143161bdbd
pawan kumar <pawan6200327812@gmail.com>
2023-09-30 12:47:04 +0530

    done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
-// const sendgrid = require('@sendgrid/mail')
 const cors = require("cors");
+const http = require("http"); // Import the http module
+const socketIo = require("socket.io"); // Import Socket.io
 const { connection } = require("./db");
 const { UserRoute } = require("./routes/user.route");
 const { DoctorRoute } = require("./routes/doctor.route");
@@ -12,83 +13,50 @@ const { AppointmentRoute } = require("./routes/appointment.route");
 const { OtpRoute } = require("./routes/otp.route");
 const { BlogRoute } = require("./routes/blog.route");
 
-
 const server = express();
+const httpServer = http.createServer(server); // Create an HTTP server
+
+const io = socketIo(httpServer); // Create a Socket.io server instance
+
 server.use(express.json());
 server.use(cors());
+// ... (use your middleware and routes)
 
-
+// Middleware
 server.use("/user", UserRoute);
 server.use("/admin", AdminRoute);
 server.use("/reviews", ReviewRoute);
 server.use("/doctors", DoctorRoute);
 server.use("/hospital", HospitalRoute);
 server.use("/appointment", AppointmentRoute);
-server.use("/otp",OtpRoute)
-server.use("/blog",BlogRoute)
-server.use(auth);
+server.use("/otp", OtpRoute);
+server.use("/blog", BlogRoute);
 
+// Socket.io connection event
+io.on("connection", (socket) => {
+  console.log("A user connected");
 
+  // You can listen for events from the client here
+  socket.on("someEventFromClient", (data) => {
+    console.log("Received data from client:", data);
 
-// server.post("/send-otp", (req, res) => {
-//     const {email} = req.body;
-    
-//   var transporter = nodemailer.createTransport({
-//     service: "gmail",
-//     auth: {
-//       user: "sharmaashish7251@gmail.com",
-//       pass: "uoyu hgfw udtp emme",
-//     },
-//   });
+    // You can emit data back to the client or broadcast it to all connected clients
+    io.emit("someEventToClient", { message: "Hello from the server" });
+  });
 
-//   var mailOptions = {
-//     from: "pawan6200327812@gmail.com",
-//     to: email,
-//     subject: "Doctors Query Appointment verifacation",
-//     text: `your varifications OTP is ${generateOTP()}`,
-//   };
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+});
 
-//   transporter.sendMail(mailOptions, function (error, info) {
-//     if (error) {
-//       console.log(error);
-//     } else {
-//       console.log("Email sent: " + info.response);
-//     }
-//   });
-// });
-
-// server.post("/verify-otp", (req, res) => {
-//   const { email, otp } = req.body;
-
-//   if (otpStore[email] && otpStore[email] === otp) {
-//     // OTP is valid
-//     res.status(200).json({ message: "OTP is valid" });
-//   } else {
-//     // OTP is invalid
-//     res.status(400).json({ error: "Invalid OTP" });
-//   }
-// });
-
-server.listen(8585, async () => {
+const PORT = process.env.PORT || 8585;
+httpServer.listen(PORT, async () => {
   try {
     await connection;
-    console.log("connected with database");
-    console.log(`server is running at port 8585`);
+    console.log("connected with the database");
+    console.log(`server is running at port ${PORT}`);
   } catch (error) {
     console.log(error);
   }
 });
-
-// all Routes
-// 1.login - http://localhost:8585/user/login - post request
-// 2.Signup - http://localhost:8585/user/signup - post request
-// 3.Signup - http://localhost:8585/admin/signup - post request
-// 4.login - http://localhost:8585/admin/login - post request
-// 5.All Doctor data - http://localhost:8585/doctors - get request
-// 6.nearest Doctor data - http://localhost:8585/doctors/nearest?lat='pass the latitude'7&lon='pass the longitude' - get request
-// 7.Add a new doctor - http://localhost:8585/doctors - post request
-// 8.Delete a doctor - http://localhost:8585/doctors/delete/id - delete request
-// 9.All Hospital data - http://localhost:8585/hospital - get request
-// 9.add a new Hospital data - http://localhost:8585/hospital - post request
-// 10.see the review  - http://localhost:8585/reviews/?doctorId = "" get request
-// 11.write a review  - http://localhost:8585/reviews/ post request
