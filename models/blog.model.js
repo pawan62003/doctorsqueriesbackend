183ea303de06bca833bfb3b6dd754ca955369019
pawan kumar <pawan6200327812@gmail.com>
2023-10-04 11:28:07 +0530

    done

--- models/blog.model.js
+++ models/blog.model.js
@@ -3,10 +3,10 @@ const mongoose = require("mongoose")
 const BlogSchema = mongoose.Schema({
     image:{type:String,required:true},
     auther:{type:String,required:true},
-    date:{type:String,required:true},
+    date:{type:String},
     description:{type:String,required:true},
     title:{type:String,required:true},
-    tag:{type:String,required:true}
+    metaUrl:{type:String,required:true}
 })
 
 const BlogModel = mongoose.model("blog",BlogSchema)
