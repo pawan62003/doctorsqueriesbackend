88658d61958af450df56fe3f69cb9b7867aa5170
pawan kumar <pawan6200327812@gmail.com>
2023-10-05 02:06:09 +0530

    done

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const http = require("http"); // Import the http module
-const socketIo = require("socket.io"); // Import Socket.io
 const { connection } = require("./db");
 const { UserRoute } = require("./routes/user.route");
 const { DoctorRoute } = require("./routes/doctor.route");
@@ -15,9 +13,7 @@ const { BlogRoute } = require("./routes/blog.route");
 const { BannerRoute } = require("./routes/banner.route");
 
 const server = express();
-const httpServer = http.createServer(server); // Create an HTTP server
 
-const io = socketIo(httpServer); // Create a Socket.io server instance
 
 server.use(express.json());
 server.use(cors());
@@ -34,26 +30,10 @@ server.use("/appointment", AppointmentRoute);
 server.use("/otp", OtpRoute);
 server.use("/blog", BlogRoute);
 
-// Socket.io connection event
-io.on("connection", (socket) => {
-  console.log("A user connected");
 
-  // You can listen for events from the client here
-  socket.on("someEventFromClient", (data) => {
-    console.log("Received data from client:", data);
-
-    // You can emit data back to the client or broadcast it to all connected clients
-    io.emit("someEventToClient", { message: "Hello from the server" });
-  });
-
-  // Handle disconnection
-  socket.on("disconnect", () => {
-    console.log("A user disconnected");
-  });
-});
 
 const PORT = process.env.PORT || 8585;
-httpServer.listen(PORT, async () => {
+server.listen(PORT, async () => {
   try {
     await connection;
     console.log("connected with the database");
